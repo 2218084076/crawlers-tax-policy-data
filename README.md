0cfe9fed8da4e7822bd496eab4ee78bf044b940b
Terry <17601306065@163.com>
2024-04-28 18:52:38 +0800

    feat: safe crawlers function is complete

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # crawlers-tax-policy-data
 
 > Get the specified field data of public announcements in the government affairs platform
+
+## Please check the development documentation before use [develop.md](docs%2Fdevelop.md)
--- src/crawlers_tax_policy_data/spider/safe_gov.py
+++ src/crawlers_tax_policy_data/spider/safe_gov.py
@@ -2,15 +2,17 @@
 国家外汇管理局
 https://www.safe.gov.cn/safe/zcfg/index.html
 """
+import asyncio
 import re
 from datetime import datetime
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
 from lxml import etree
 
 from crawlers_tax_policy_data.config import settings
 from crawlers_tax_policy_data.spider.base import BaseSpider
+from crawlers_tax_policy_data.storage.local import save_data
 from crawlers_tax_policy_data.utils.utils import clean_text
 
 
@@ -26,6 +28,8 @@ class SafeSpider(BaseSpider):
         self.output_dir = Path(
             settings.GOV_OUTPUT_PAHT
         ) / self.folder / f'{self.start_date}-{self.end_date}'
+        self.timestamp_format = '%Y-%m-%d'
+        self.url_prefix = 'https://www.safe.gov.cn'
 
     @property
     def url(self):
@@ -86,12 +90,19 @@ class SafeSpider(BaseSpider):
         await self.init_page()
         await self.page.goto(self.url)
         await self.page.wait_for_timeout(350)
-        html_text = await self.page.content()
         self.logger.info('get %s', self.page)
 
-        detail_page_li = await self.list_page_parser(html_text)
+        detail_page_li = await self.list_page_parser()
 
-    async def list_page_parser(self, html_text: str):
+        if not detail_page_li:
+            self.logger.warning(
+                f'No data found for 国家外汇管理局 %s for dates %s-%s', self.url, self.start_date, self.end_date
+            )
+
+        for pg in detail_page_li:
+            await self.details_pg_processor(pg_data=pg)
+
+    async def list_page_parser(self):
         """
         parse news list
         :param html_text:
@@ -102,9 +113,113 @@ class SafeSpider(BaseSpider):
         html = etree.HTML(html_text, etree.HTMLParser(encoding="utf-8"))
 
         li_list: List[etree._Element] = html.xpath('//div[@class="list_conr"]//li')
+        res.extend(self.per_line_parser(li_list))
+
+        last_pg_date_str = ''.join(li_list[-1].xpath('./dd/text()')).strip()
+        last_pg_date = datetime.strptime(last_pg_date_str, '%Y-%m-%d').date()
+
+        while last_pg_date >= datetime.strptime(self.start_date, '%Y-%m-%d').date():
+            await self.page.locator('//a[@title="下一页"]').click()
+            await self.page.wait_for_timeout(350)
+            next_pg_text = await self.page.content()
+
+            next_html: etree._Element = etree.HTML(
+                next_pg_text,
+                etree.HTMLParser(encoding="utf-8", remove_comments=False)
+            )
+            nex_li_list: List[etree._Element] = next_html.xpath('//div[@class="list_conr"]//li')
+            res.extend(self.per_line_parser(nex_li_list))
+
+            _last_pg_date_str = ''.join(nex_li_list[-1].xpath('./dd/text()')).strip()
+            last_pg_date = datetime.strptime(_last_pg_date_str, '%Y-%m-%d').date()
+        self.logger.info('There are %s proclamation items', len(res))
         return res
 
-    def per_line_parser(self, li_list_html: List[etree._Element]):
+    async def details_pg_processor(self, pg_data: dict):
+        """
+        Detail page processor
+
+        Parse each detail page to extract news data
+        The `pg_data` format is
+        {
+            'title': _title,
+            'link': _link,
+            'date': _page_date,
+        }
+        :param pg_data:
+        :return:
+        """
+        _link = pg_data['link']
+
+        if '.pdf' in _link:
+            pg_content = {key: pg_data[key] for key in ['link', 'date', 'title']}
+
+        else:
+            repo = await self.async_get_req(
+                url=_link,
+                headers=self.headers
+            )
+            repo.encoding = 'utf-8'
+            html_text = repo.text
+
+            pg_content = self.details_pg_parser(html_text=html_text)
+
+            _title = re.sub(r'\s+', '', pg_data["title"])
+            detail_page_html_file = self.output_dir / f'''{pg_data['date']}-{_title}.html'''
+            self.save_html(
+                html_content=html_text,
+                file=detail_page_html_file
+            )
+
+            pg_content.update({key: pg_data[key] for key in ['link', 'date', 'title']})
+            pg_content.update({'html_file': str(detail_page_html_file)})
+
+        save_data(
+            content=pg_content,
+            file_path=self.output_dir / f"{self.start_date}-{self.end_date}-public"
+                                        f"-information.csv"
+        )
+        await asyncio.sleep(0.5)
+
+    def details_pg_parser(
+            self,
+            html_text: str
+    ):
+        """
+        deatil page parser
+        :param html_text:
+        :return:
+        """
+        _url_prefix = 'https://www.safe.gov.cn'
+
+        file_xpath = self.build_file_xpath()
+        xpath_query = f'//a[{file_xpath}]'
+
+        html: etree._Element = etree.HTML(
+            html_text,
+            etree.HTMLParser(encoding="utf-8")  # fix garbled characters in requests
+        )
+        title = ''.join(html.xpath('//div[@class="detail_tit"]//text()')).strip()
+
+        texts = html.xpath('//div[@id="content"]//span/text()')
+        cleaned_texts = [clean_text(text) for text in texts]
+        editor = ''.join(html.xpath('//span[@id="wh"]//text()')).strip()
+
+        all_related_links = extract_related_links(html, '//div[@class="list_conr"]//a', _url_prefix)
+
+        all_appendix = extract_related_links(html, xpath_query, _url_prefix)
+        return {
+            'title': title,
+            'text': '\n'.join(cleaned_texts).strip(),
+            'editor': editor,
+            'appendix': ',\n'.join(all_appendix).replace('\xa0', ''),
+            'related_documents': ',\n'.join(list(set(all_related_links))),
+        }
+
+    def per_line_parser(
+            self,
+            li_list_html: List[etree._Element]
+    ) -> List[Dict[str, str]]:
         """
         Per-line parser
 
@@ -113,63 +228,27 @@ class SafeSpider(BaseSpider):
         :return:
         """
         res = []
-        prefix = 'https://www.sc.gov.cn'
+        prefix = 'https://www.safe.gov.cn'
         for line in li_list_html:
-            _page_date = ''.join(line.xpath('.//div[@class="lie5"]//text()')).strip()
-            _date = datetime.strptime(
-                _page_date,
-                '%Y-%m-%d'
-            ).date()
-            if (datetime.strptime(self.start_date, '%Y-%m-%d').date()
+            _page_date = ''.join(line.xpath('./dd/text()')).strip()
+            try:
+                _date = datetime.strptime(
+                    _page_date,
+                    self.timestamp_format
+                ).date()
+            except ValueError:
+                continue
+            if (datetime.strptime(self.start_date, self.timestamp_format).date()
                     <= _date
-                    <= datetime.strptime(self.end_date, '%Y-%m-%d').date()):
-                _title = ''.join(line.xpath('.//div[@class="lie2"]//text()')).strip()
-                _link = f'''{prefix}{''.join(line.xpath('.//div[@class="lie2"]/a/@href')).strip()}'''
-                _editor = ''.join(line.xpath('.//div[@class="lie3"]//text()')).strip()
+                    <= datetime.strptime(self.end_date, self.timestamp_format).date()):
+                _title = ''.join(line.xpath('.//a//text()')).strip()
+                _link = f'''{prefix}{''.join(line.xpath('.//a/@href')).strip()}'''
 
                 res.append({
                     'title': _title,
                     'link': _link,
                     'date': _page_date,
-                    'editor': _editor,
                 })
-
-        return res
-
-    def parse_detail_page(self, html_text: str):
-        """
-        parse detail page
-        :param html_text:
-        :return:
-        """
-        res = {}
-        self.logger.info('parse detail_page text data')
-
-        file_xpath = self.build_file_xpath()
-        xpath_query = f'//a[{file_xpath}]'
-
-        html = etree.HTML(html_text, etree.HTMLParser(encoding="utf-8"))  # fix garbled characters in requests
-        title = re.sub(r'\s+', '', clean_text(''.join(html.xpath('//div[@id="ivs_title"]/text()'))))
-        # 文号
-        editor = clean_text(''.join(html.xpath('//div[@id="ivs_content"]/p[2]/text()'))) if self.is_match(
-            ''.join(html.xpath('//div[@id="ivs_content"]/p[2]/text()'))) else ''
-
-        related_documents = ',\n'.join(
-            [' - '.join(
-                (''.join(item.xpath('./a/@title')), 'https://www.shanghai.gov.cn' + ''.join(item.xpath('./a/@href'))))
-                for item in
-                html.xpath('//div[@class="newsbox"]/ul//li')]
-        )
-        # 附件
-        all_appendix = self.extract_links(html, xpath_query)
-        res.update({
-            'title': title,
-            'text': title + clean_text(''.join(html.xpath('//div[@id="xggj"]//text()'))),
-            'editor': editor,
-            'appendix': ',\n'.join(all_appendix).replace('\xa0', ''),
-            'related_documents': related_documents,
-        })
-
         return res
 
     async def run(self):
@@ -180,15 +259,14 @@ class SafeSpider(BaseSpider):
         self.logger.info('start running crawlers...')
         await self.get_public_info()
 
-    @staticmethod
-    def extract_links(html, xpath):
-        """
-        Extract the links according to the given XPath
-        :param html:
-        :param xpath:
-        :return:
-        """
-        res = [''.join(link.xpath('./text()')).strip() + ' https://www.shanghai.gov.cn' + ''.join(
-            link.xpath('@href')).strip() for link
-               in html.xpath(xpath)]
-        return list(set(res))
+
+def extract_related_links(html, xpath, prefix: str):
+    """
+    Extract the links according to the given XPath
+    :param prefix:
+    :param html:
+    :param xpath:
+    :return:
+    """
+    return [f'{"".join(list(set(link.xpath(".//text()"))))} {prefix}{"".join(link.xpath("@href"))}'.strip() for link in
+            html.xpath(xpath)]
